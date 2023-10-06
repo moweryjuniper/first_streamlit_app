ffe3451969eabba35d1176449214bcbca89c68f8
moweryjuniper <147108808+moweryjuniper@users.noreply.github.com>
2023-10-06 10:05:24 -0400

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,9 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 #streamlit.header('Breakfast Menu') --streamlit has no attribute menu
 #streamlit.body('Breakfast Menu') --streamlit has no attribute body
+# 🥣 🥗 🐔 🥑🍞
 streamlit.header('Breakfast Menu') 
-streamlit.text('Blueberry Oatmeal') 
-streamlit.text('Kale and Spinach Smoothie') 
-streamlit.text('Breakfast Sandwich') 
+streamlit.text('🥣 Blueberry Oatmeal') 
+streamlit.text('🥗 Kale and Spinach Smoothie') 
+streamlit.text('🐔 Breakfast Sandwich') 
+streamlit.text('🥑🍞 Avocado Toast')
