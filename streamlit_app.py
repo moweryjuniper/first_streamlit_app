af0df0fb0ce4e5844914b2b4ff1b19045ef54e57
moweryjuniper <147108808+moweryjuniper@users.noreply.github.com>
2023-10-18 17:17:53 -0400

    Update streamlit_app.py
    url error imports and stop streamlit for error resolutions.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import snowflake.connector
 import requests
 import pandas
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 # 🥣 🥗 🐔 🥑🍞
@@ -33,6 +34,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_cho
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do? Puts that normalized variable in a dataframe
 streamlit.dataframe(fruityvice_normalized)
+
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
