16072e4c56ce1b97435f2cacdd821b921a46b789
moweryjuniper <147108808+moweryjuniper@users.noreply.github.com>
2023-10-18 17:55:14 -0400

    Update streamlit_app.py
    indent for return of the get_fruit_load_list function

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * FROM pc_rivery_db.public.fruit_load_list")
-  return my_cur.fetchall()
+    return my_cur.fetchall()
 
 #adding a button to load the fruit
 if streamlit.button('Get fruit load list'):
